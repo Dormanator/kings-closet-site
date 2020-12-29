fd122da73f4ffad533ead3a1508b72d29629c03b
Dormanator <ryandormanthe1@gmail.com>
2020-12-29 10:02:42 -0600

    Updated contact section to info instead of form

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,11 +1,11 @@
 import React from "react"
-import Button from "react-bootstrap/Button"
 import Col from "react-bootstrap/Col"
 import Container from "react-bootstrap/Container"
-import Form from "react-bootstrap/Form"
 import Row from "react-bootstrap/Row"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faPhone, faEnvelope } from "@fortawesome/free-solid-svg-icons"
 
-const Contact = ({ title, paragraphs }) => {
+const Contact = ({ title, paragraphs, phone, email, address }) => {
   return (
     <div id="contact" className="bg-secondary pt-4 pb-5">
       <Container>
@@ -25,57 +25,38 @@ const Contact = ({ title, paragraphs }) => {
             ))}
           </Col>
         </Row>
-        <Row className="pt-3">
-          <Col md={{ span: 8, offset: 2 }}>
-            <Form onSubmit={() => console.log("Submitted")}>
-              <Row>
-                <Col md="6">
-                  <Form.Group controlId="formName">
-                    <Form.Control
-                      type="text"
-                      placeholder="Name"
-                      aria-label="Name"
-                    />
-                  </Form.Group>
-                </Col>
-                <Col md="6">
-                  <Form.Group controlId="formEmail">
-                    <Form.Control
-                      type="email"
-                      placeholder="Email"
-                      aria-label="Email"
-                    />
-                    <Form.Text className="text-white">
-                      We'll never share your email with anyone else.
-                    </Form.Text>
-                  </Form.Group>
-                </Col>
-              </Row>
-              <Row>
-                <Col>
-                  <Form.Group controlId="formMessage">
-                    <Form.Control
-                      as="textarea"
-                      rows={4}
-                      placeholder="Message"
-                      aria-label="Message"
-                    />
-                  </Form.Group>
-                </Col>
-              </Row>
-              <Row>
-                <Col className="text-center">
-                  <Button
-                    variant="primary"
-                    size="lg"
-                    type="submit"
-                    className="text-white"
-                  >
-                    Get in Touch!
-                  </Button>
-                </Col>
-              </Row>
-            </Form>
+        <Row className="pt-5">
+          <Col md="6">
+            <Row>
+              <Col className="d-flex align-items-center">
+                <FontAwesomeIcon
+                  icon={faPhone}
+                  size="3x"
+                  className="text-primary mr-4"
+                />
+                <h2 className="h4 text-white font-weight-normal">{phone}</h2>
+              </Col>
+            </Row>
+            <Row className="mt-5">
+              <Col className="d-flex align-items-center">
+                <FontAwesomeIcon
+                  icon={faEnvelope}
+                  size="3x"
+                  className="text-primary mr-4"
+                />
+                <h2 className="h4 text-white font-weight-normal">{email}</h2>
+              </Col>
+            </Row>
+          </Col>
+          <Col md="6">
+            <iframe
+              title="King's Closet's Location"
+              frameborder="0"
+              style={{ border: "none" }}
+              src={`https://www.google.com/maps/embed/v1/place?key=${
+                process.env.GATSBY_MAPS_API_KEY
+              }&q=${encodeURI(address)}`}
+            />
           </Col>
         </Row>
       </Container>
@@ -88,6 +69,9 @@ Contact.defaultProps = {
   paragraphs: [
     "Do you want to schedule a donation drop-off or a time to volunteer? Want to find out how you can help?",
   ],
+  email: "contact@kingscloset.org",
+  phone: "214 478 7087",
+  address: "18492 FM2755, Fate, TX 75189",
 }
 
 export default Contact
